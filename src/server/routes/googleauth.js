65935cd3147e0d0d80e3d0147fb85f0c9faa781c
Angela Tom <tangela88@gmail.com>
2022-10-13 07:53:10 -0400

    Successfully made google drive api call for listing files

--- src/server/routes/googleauth.js
+++ src/server/routes/googleauth.js
@@ -2,6 +2,7 @@ var express = require('express');
 const { OAuth2Client, auth } = require('google-auth-library');
 const router = express.Router();
 const { google } = require('googleapis');
+const { token } = require('morgan');
 const { redirect } = require('react-router-dom');
 const oauth2 = google.oauth2('v2');
 
@@ -24,7 +25,13 @@ function getConnectionUrl() {
       prompt: 'consent',
       scope: ['https://www.googleapis.com/auth/drive.metadata.readonly',
         'https://www.googleapis.com/auth/userinfo.email',
-        'https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/drive'
+        'https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/drive', 
+        'https://www.googleapis.com/auth/drive.appdata',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/drive.metadata',
+        'https://www.googleapis.com/auth/drive.metadata.readonly',
+        'https://www.googleapis.com/auth/drive.photos.readonly',
+        'https://www.googleapis.com/auth/drive.readonly'
     ]
   });
 }
@@ -40,15 +47,14 @@ router.get('/authorize', async function (req, res, next) {
   console.log(code);
   const {tokens} = await Oauth2Client.getToken(code)
   Oauth2Client.setCredentials(tokens);
-  listFiles(OAuth2Client);
-  res.send("HI");
+  listFiles(OAuth2Client, tokens.access_token);
+  res.send("Hello");
 });
 
 router.get('/files', function (req, res, next) {
   let authClient = OAuth2Client;
-  console.log("AUTH CLIENT");
-  console.log(authClient);
-  listFiles(authClient);
+  let tokens = authClient.access_token
+  listFiles(OAuth2Client, tokens.access_token);
   res.send("files");
 });
 
@@ -56,10 +62,10 @@ router.get('/files', function (req, res, next) {
  * Lists the names and IDs of up to 10 files.
  * @param {OAuth2Client} authClient An authorized OAuth2 client.
  */
-async function listFiles(authClient) {
+async function listFiles(authClient, tokens) {
   const drive = google.drive({version: 'v3', auth: authClient});
   const res = await drive.files.list({
-    key: "AIzaSyB_vwp4J8ASKdS7PSwgjOkJRDukxNSP9L4"
+    access_token: tokens
   });
   const files = res.data.files;
   if (files.length === 0) {
