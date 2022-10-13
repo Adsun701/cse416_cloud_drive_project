1354e7fb56a8a37d545076629730bcd293efb29f
Andrew Sun <adsun701@gmail.com>
2022-10-13 18:44:49 -0400

    change format from class to module exports for router

--- src/server/model/snapshots/GoogleFileSharingSnapshot.js
+++ src/server/model/snapshots/GoogleFileSharingSnapshot.js
@@ -1,38 +1,49 @@
 const { authenticate } = require('@google-cloud/local-auth');
+var express = require('express');
+var router = express.Router();
 const { google } = require('googleapis');
 const Schema = mongoose.Schema
 const FileSnapshot = require('../file-snapshot-model')
 
-class GoogleFileSharingSnapshot {
-	client = null;
-	drive = null;
+var fetch = require('./fetch');
+var { GRAPH_ME_ENDPOINT } = require('../authConfig');
 
-	GoogleFileSharingSnapshot(client) {
-		this.drive = google.drive({version: 'v3', auth: client});
-		this.client = client;
-	}
-    
-    takeSnapshot = () => {
-		snapshotMap = new Map();
-		drive.files.list({ }, (err, res) => {
-			if (err) {
-				//console.error('The API returned an error.');
-				throw err;
-			}
-			const files = res.data.files;
-			if (files.length === 0) {
-				//console.log('No files found.');
-				return null;
-			} else {
-				//console.log('Files Found!');
-				for (const file of files) {
-					snapshotMap.set(file.id, file.permissions);
-				}
-			}
-		})
-		fileSnapshot = new FileSnapshot({files: snapshotMap});
-		fileSnapshot.save();
+// custom middleware to check auth state
+function isAuthenticated(req, res, next) {
+    if (!req.session.isAuthenticated) {
+        return res.redirect('/auth/signin'); // redirect to sign-in route
     }
-}
+    next();
+};
 
-export { GoogleFileSharingSnapshot };
+router.get('/snapshot/file/google',
+	isAuthenticated, // check if user is authenticated
+    async function (req, res, next) {
+        try {
+			snapshotMap = new Map();
+			drive.files.list({ access_token: req.session.googleToken }, (err, res) => {
+				if (err) {
+					//console.error('The API returned an error.');
+					throw err;
+				}
+				const files = res.data.files;
+				if (files.length === 0) {
+					//console.log('No files found.');
+					return null;
+				} else {
+					//console.log('Files Found!');
+					for (const file of files) {
+						snapshotMap.set(file.id, file.permissions);
+					}
+				}
+			})
+			fileSnapshot = new FileSnapshot({files: snapshotMap});
+			fileSnapshot.save();
+		}
+		catch (error) {
+			next(error);
+		}
+    }
+);
+
+module.exports = router;
--- src/server/model/snapshots/GoogleGroupMembershipSnapshot.js
+++ src/server/model/snapshots/GoogleGroupMembershipSnapshot.js
@@ -1,47 +1,56 @@
 const { authenticate } = require('@google-cloud/local-auth');
+var express = require('express');
+var router = express.Router();
 const { google } = require('googleapis');
 const Schema = mongoose.Schema
 const GroupSnapshot = require('../group-snapshot-model')
 
-class GoogleGroupMembershipSnapshot {
-    client = null;
-	drive = null;
+var fetch = require('./fetch');
+var { GRAPH_ME_ENDPOINT } = require('../authConfig');
 
-	GoogleGroupMembershipSnapshot(client) {
-		this.drive = google.drive({version: 'v3', auth: client});
-		this.client = client;
-	}
-
-    // gets file list, gets names of user emails in sharing group for each file, and returns file name along with each email for each line.
-    takeSnapshot = () => {
-		snapshotMap = new Map();
-		drive.files.list({ }, (err, res) => {
-			if (err) {
-				//console.error('The API returned an error.');
-				throw err;
-			}
-			const files = res.data.files;
-			if (files.length === 0) {
-				//console.log('No files found.');
-				return null;
-			} else {
-				//console.log('Files Found!');
-				for (const file of files) {
-					permissions = file.permissions;
-					permissionAddresses = new Array();
-					for (permission in permissions) {
-						if (permission.type == "group") {
-							permissionAddresses.push(permission.emailAddress);
-						}
-					}
-					snapshotMap.set(file.id, permissionAddresses);
-				}
-			}
-		})
-		groupSnapshot = new GroupSnapshot({files: snapshotMap});
-		groupSnapshot.save();
+// custom middleware to check auth state
+function isAuthenticated(req, res, next) {
+    if (!req.session.isAuthenticated) {
+        return res.redirect('/auth/signin'); // redirect to sign-in route
     }
+    next();
+};
 
-}
+router.get('/snapshot/group/google',
+	isAuthenticated, // check if user is authenticated
+    async function (req, res, next) {
+        try {
+			snapshotMap = new Map();
+			drive.files.list({ access_token: req.session.googleToken }, (err, res) => {
+				if (err) {
+					//console.error('The API returned an error.');
+					throw err;
+				}
+				const files = res.data.files;
+				if (files.length === 0) {
+					//console.log('No files found.');
+					return null;
+				} else {
+					//console.log('Files Found!');
+					for (const file of files) {
+						permissions = file.permissions;
+						permissionAddresses = new Array();
+						for (permission in permissions) {
+							if (permission.type == "group") {
+								permissionAddresses.push(permission.emailAddress);
+							}
+						}
+						snapshotMap.set(file.id, permissionAddresses);
+					}
+				}
+			})
+			groupSnapshot = new GroupSnapshot({files: snapshotMap});
+			groupSnapshot.save();
+		}
+		catch (error) {
+			next(error);
+		}
+    }
+);
 
-export { GoogleGroupMembershipSnapshot };
+module.exports = router;
