a8c36cecea2bddc15d69651f193dd15959eef660
Andrew Sun <adsun701@gmail.com>
2022-10-21 17:06:27 -0400

    use multer for upload

--- src/server/routes/googlegroupsnapshot.js
+++ src/server/routes/googlegroupsnapshot.js
@@ -1,9 +1,12 @@
 // const { authenticate } = require('@google-cloud/local-auth');
 const express = require('express');
 
+const fs = require('fs');
 const router = express.Router();
+const multer  = require('multer');
 const { google } = require('googleapis');
 const GroupSnapshot = require('../model/group-snapshot-model');
+const upload = multer({ dest: '../../public/data/uploads/' });
 
 // custom middleware to check auth state
 // eslint-disable-next-line consistent-return
@@ -16,12 +19,18 @@ function isAuthenticated(req, res, next) {
 
 router.post(
   '/snapshot',
+  upload.single('memberpagehtml'),
   isAuthenticated, // check if user is authenticated
   async (req, res, next) => {
     const groupname = req.body.groupname;
     const groupaddress = req.body.groupaddress;
     const timestamp = req.body.timestamp;
-    const memberpagehtml = req.body.memberpagehtml; // use "Profile image for " as indexOf
+    //console.log(req.file);
+    let memberpagehtml = fs.createReadStream(req.file.path, 'utf8'); // use "Profile image for " as indexOf
+    console.log("groupname is : " + groupname);
+    console.log("groupaddress is : " + groupaddress);
+    console.log("timestamp is : " + timestamp);
+    console.log("memberpagehtml is : " + memberpagehtml);
 
     const memberarray = new Array();
     let start = 0;
